a6eaf0281bf8912c4a722db8fefb7c1859cbd995
hrammal15 <rammal1@uwindsor.ca>
2020-12-09 23:20:19 -0500

    Started from scratch

--- projectZero/src/main/java/com/revature/services/UserService.java
+++ projectZero/src/main/java/com/revature/services/UserService.java
@@ -1,5 +1,18 @@
 package com.revature.services;
 
+import com.revature.models.User;
+
 public class UserService {
+	
+	public static User login(String username, String password) {
+		
+		return null;
+	}
+	
+	public User register(String fname, String lname, String username) {
+		
+		return null;
+	}
+
 
 }
